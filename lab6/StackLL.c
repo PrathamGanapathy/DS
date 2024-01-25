123eb082ce73d44d6ab9474e0f796e12d8ee8db2
PrathamGanapathy <153417541+PrathamGanapathy@users.noreply.github.com>
2024-01-25 12:47:11 +0530

    Create StackLL.c

---
+++ lab6/StackLL.c
@@ -0,0 +1,70 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct Node{
+    int data;
+    struct Node *next;
+} node;
+
+node* head = NULL;
+int count = 0;
+
+void insert(int data);
+int delete();
+void display();
+
+int main(){
+    int data, choice, pos;
+    printf("1. Insert\n2. Delete\n3. Exit\nChoice: ");
+    scanf("%d", &choice);
+    while(choice != 3){
+        if (choice == 1){
+            printf("Enter data: ");
+            scanf("%d", &data);
+            insert(data);
+            printf("Count: %d\n", count);
+        } else if (choice == 2){
+            printf("Integer popped = %d\n", delete());
+            printf("Count: %d\n", count);
+        }
+        display();
+        printf("Enter choice: ");
+        scanf("%d", &choice);
+    }
+
+
+    return 0;
+}
+
+void insert(int data){
+    node* new_node = (node*)malloc(sizeof(node));
+    new_node->data = data;
+    new_node->next = head;
+    head = new_node;
+    count++;
+    return;
+
+}
+
+int delete(){
+    node* temp = head;
+    head = head->next;
+    int t = temp->data;
+    free(temp);
+    count--;
+    return t;
+
+}
+
+void display(){
+    node* temp = head;
+    printf("Stack: ");
+    while (temp->next != NULL){
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    printf("%d ", temp->data);
+    printf("\n");
+}
